27cf38173e3439a9d0490936d25f94b775fb7634
Mike Cornish <cornishmw@gmail.com>
2018-08-18 18:02:18 -0400

    Adds PropTypes

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 class Login extends Component {
-    constructor(props) {
-        super(props);
+    static propTypes = {
+        url: PropTypes.string
     }
+
     render() {
         return (
             <a role="button" href={this.props.url}>Sign In To YNAB</a>
